37ee5106408768e5271573865d335862e190324b
Brandon Burton <brandon@inatree.org>
2014-10-03 17:06:51 -0700

    v0.2.4 - adding support for JAVA_SERVICE_NAME
    - added a JAVA_SERVICE_NAME to bin/storm, which means shipping
      a custom copy of the storm python script until I get get it
      submitted upstream to the storm project
    - Added the JAVA_SERVICE_NAME environment variable to the upstart
      template
    - Added custom bin/storm to config.rb

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "colin.surprenant@gmail.com"
 license          "MIT License"
 description      "installs and configures Twitter Storm"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.3"
+version          "0.2.4"
 name             "storm"
 
 depends "logrotate"
--- recipes/config.rb
+++ recipes/config.rb
@@ -5,3 +5,10 @@ template "Storm conf file" do
   group node[:storm][:deploy][:group]
   mode 0644
 end
+
+template "/home/#{node.storm.deploy.user}/apache-storm-#{node.storm.version}/bin/storm" do
+  source "bin_storm.py.erb"
+  owner node[:storm][:deploy][:user]
+  group node[:storm][:deploy][:group]
+  mode 0755
+end
